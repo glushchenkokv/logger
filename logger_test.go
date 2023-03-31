f5807f62b0050f795ec7b56de1a94313afcfa428
glushchenkokv <glushchenkokv@gmail.com>
2023-03-31 23:14:07 +0600

    first commit

---
+++ logger_test.go
@@ -0,0 +1,71 @@
+package logger
+
+import (
+	"testing"
+)
+
+func TestDefaultConfigLogger(t *testing.T) {
+	log := NewLogger(Config{})
+
+	log.Trace("Something very low level.")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+}
+
+func TestTextLogger(t *testing.T) {
+	log := NewLogger(Config{
+		Level:            "trace",
+		Format:           "text",
+		ReportCaller:     false,
+		DisableTimestamp: false,
+	})
+
+	log.Trace("Something very low level.")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+}
+
+func TestJsonLogger(t *testing.T) {
+	log := NewLogger(Config{
+		Level:            "trace",
+		Format:           "json",
+		ReportCaller:     false,
+		DisableTimestamp: false,
+	})
+
+	log.Trace("Something very low level.")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+}
+
+func TestFatalLogger(t *testing.T) {
+	t.Skip()
+	log := NewLogger(Config{
+		Level:            "trace",
+		Format:           "json",
+		ReportCaller:     false,
+		DisableTimestamp: false,
+	})
+
+	// Calls os.Exit(1) after logging
+	log.Fatal("Bye.")
+}
+
+func TestPanicLogger(t *testing.T) {
+	t.Skip()
+	log := NewLogger(Config{
+		Level:            "trace",
+		Format:           "json",
+		ReportCaller:     false,
+		DisableTimestamp: false,
+	})
+
+	// Calls panic() after logging
+	log.Panic("I'm bailing.")
+}
